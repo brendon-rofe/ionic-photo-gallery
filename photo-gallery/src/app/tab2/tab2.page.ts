50632bed4f5d09f9e9d459d0cfaf04998af5b7d0
Brendon Rofe <brofe93@gmail.com>
2025-11-04 10:50:40 +0200

    fix: Use @for instead of NgFor, since NgFor is depricated

--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -3,13 +3,12 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonFabButton, Ion
 import { addIcons } from 'ionicons';
 import { camera } from 'ionicons/icons';
 import { PhotoService } from '../services/photo.service';
-import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonFabButton, IonFab, IonGrid, IonRow, IonCol, IonImg, NgFor]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonFabButton, IonFab, IonGrid, IonRow, IonCol, IonImg]
 })
 export class Tab2Page {
 
